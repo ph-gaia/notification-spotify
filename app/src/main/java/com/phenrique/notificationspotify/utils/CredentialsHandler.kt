a2a9caab69729f5d1929ff8fe23082a5ca4b6d17
Paulo Henrique Coelho Gaia <paulo.coelho@webjump.com.br>
2020-09-22 22:50:07 -0300

    Added credentialsHandle

---
+++ app/src/main/java/com/phenrique/notificationspotify/utils/CredentialsHandler.kt
@@ -0,0 +1,45 @@
+package com.phenrique.notificationspotify.utils
+
+import android.content.Context
+import android.content.SharedPreferences
+import java.util.concurrent.TimeUnit
+
+
+class CredentialsHandler {
+    private val ACCESS_TOKEN_NAME = "webapi.credentials.access_token"
+    private val ACCESS_TOKEN = "access_token"
+    private val EXPIRES_AT = "expires_at"
+
+    fun setToken(
+        context: Context,
+        token: String?,
+        expiresIn: Long,
+        unit: TimeUnit
+    ) {
+        val appContext = context.applicationContext
+        val now = System.currentTimeMillis()
+        val expiresAt = now + unit.toMillis(expiresIn)
+        val sharedPref = getSharedPreferences(appContext)
+        val editor = sharedPref.edit()
+        editor.putString(ACCESS_TOKEN, token)
+        editor.putLong(EXPIRES_AT, expiresAt)
+        editor.apply()
+    }
+
+    private fun getSharedPreferences(appContext: Context): SharedPreferences {
+        return appContext.getSharedPreferences(
+            ACCESS_TOKEN_NAME,
+            Context.MODE_PRIVATE
+        )
+    }
+
+    fun getToken(context: Context): String? {
+        val appContext = context.applicationContext
+        val sharedPref = getSharedPreferences(appContext)
+        val token = sharedPref.getString(ACCESS_TOKEN, null)
+        val expiresAt = sharedPref.getLong(EXPIRES_AT, 0L)
+        return if (token == null || expiresAt < System.currentTimeMillis()) {
+            null
+        } else token
+    }
+}
