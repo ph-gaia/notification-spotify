966ddf5ee82f0358d33f704e16ee25430b0f3d60
Paulo Henrique Coelho Gaia <paulo.coelho@webjump.com.br>
2020-09-22 22:31:03 -0300

    first commit

---
+++ app/src/main/java/com/phenrique/notificationspotify/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.phenrique.notificationspotify
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
